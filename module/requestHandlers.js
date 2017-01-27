f869d4a7ba79f0bd16c24543c732e8435b1c6c75
ChaunceyKiwi <chaunceykiwi@gmail.com>
2017-01-27 12:20:58 -0800

    blocking mode

--- module/requestHandlers.js
+++ module/requestHandlers.js
@@ -1,5 +1,13 @@
 function start() {
   console.log("Request handler 'start' was called");
+
+  function sleep(milliSeconds) {
+    var startTime = new Date().getTime();
+    while (new Date().getTime() < startTime + milliSeconds);
+  }
+
+  // Blocking function
+  sleep(10000);
   return "Hello Start";
 }
 

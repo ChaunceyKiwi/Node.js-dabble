1b14b0251c1cf4dbe626e35555c6947d81bbb322
ChaunceyKiwi <chaunceykiwi@gmail.com>
2017-01-27 12:10:43 -0800

    update version to TNBB page37

--- module/server.js
+++ module/server.js
@@ -1,18 +1,19 @@
-// requires the http module that ships with Node.js and
-// makes it accessible through the variable http
 var http = require("http");
+var url = require("url");
 
-// call one of the functions the http module offers: createServer
-// This function returns an object, this object has a method named listen
-// In JavaScript, functions can be passed around like any other value.
+function start(route, handle) {
+  function onRequest(request, response) {
+    var pathName = url.parse(request.url).pathname;
+    console.log("Request for " + pathName + " received.");
 
-function start() {
-    http.createServer(function(request, response) {
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write("Hello World");
+    var content = route(handle, pathName);
+    response.write(content);
     response.end();
-  }).listen(8888);
+  }
+
+  http.createServer(onRequest).listen(8888);
+  console.log("Server has started");
 }
 
-console.log("Server has started");
 exports.start = start;

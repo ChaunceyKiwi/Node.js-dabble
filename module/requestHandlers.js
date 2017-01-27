a143d1b1599530b292905cd081b2b4adcbb2c826
ChaunceyKiwi <chaunceykiwi@gmail.com>
2017-01-27 15:32:11 -0800

    tutorial up to page54

--- module/requestHandlers.js
+++ module/requestHandlers.js
@@ -1,19 +1,32 @@
-function start() {
+var exec = require("child_process").exec;
+var querystring = require("querystring");
+
+function start(response, postData) {
   console.log("Request handler 'start' was called");
+  var body = '<html>'+
+   '<head>'+
+   '<meta http-equiv="Content-Type" content="text/html; '+
+   'charset=UTF-8" />'+
+   '</head>'+
+   '<body>'+
+   '<form action="/upload" method="post">'+
+   '<textarea name="text" rows="20" cols="60"></textarea>'+
+   '<input type="submit" value="Submit text" />'+
+   '</form>'+
+   '</body>'+
+   '</html>';
 
-  function sleep(milliSeconds) {
-    var startTime = new Date().getTime();
-    while (new Date().getTime() < startTime + milliSeconds);
-  }
-
-  // Blocking function
-  sleep(10000);
-  return "Hello Start";
+  response.writeHead(200, {"Content-Type": "text/html"});
+  response.write(body);
+  response.end();
 }
 
-function upload() {
+function upload(response, postData) {
   console.log("Request handler 'upload' was called");
-  return "Hello Upload";
+  response.writeHead(200, {"Content-Type": "text/plain"});
+  console.log(querystring.parse(postData));
+  response.write("You have sent the text: " + querystring.parse(postData).text);
+  response.end();
 }
 
 exports.start = start;
